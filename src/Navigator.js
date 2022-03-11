fde0312bb3febf30adc466c40ed267c5e4aff7c6
Erin Falejczyk <efalejczyk3@gatech.edu>
2022-03-10 22:32:35 -0500

    add parental and car win pages

--- src/Navigator.js
+++ src/Navigator.js
@@ -46,6 +46,8 @@ import FallsR from "./screens/FallsR";
 import FallsR1 from "./screens/FallsR1";
 import RoadRev1 from "./screens/RoadRev1";
 import ParRev1 from "./screens/ParRev1";
+import CarW from "./screens/CarW";
+import ParW from "./screens/ParW";
 
 import LevelOne from "./screens/Level1";
 import LevelTwo from "./screens/Level2";
@@ -126,6 +128,13 @@ const screens = {
         animationEnabled: false,
       },
     },
+    CarW: {
+      screen: CarW,
+      navigationOptions: {
+        headerShown: false,
+        animationEnabled: false,
+      },
+    },
     ParentalHealthMM: {
         screen: ParentalHealthMM,
         navigationOptions: {
@@ -135,6 +144,13 @@ const screens = {
     },
     ParRev1: {
       screen: ParRev1,
+      navigationOptions: {
+        headerShown: false,
+        animationEnabled: false,
+      },
+    },
+    ParW: {
+      screen: ParW,
       navigationOptions: {
         headerShown: false,
         animationEnabled: false,
