c050988212d0855eacad5fc1befb9da4edb460a8
ckantor8 <ckantor8@gatech.edu>
2022-10-10 20:52:37 -0400

    Login completely functional, game scores added to user metadata; figured out how to query all users' data, just need to figure out how to get current user data to implement global leaderboard

---
+++ src/screens/GlobalBoard.js
@@ -0,0 +1,141 @@
+import React, {Component} from "react";
+import { ImageBackground, Linking, ScrollView, StyleSheet, Text, TouchableOpacity, View, SafeAreaView, FlatList } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import Background from "../assets/app/bg.png";
+import BackButton from "../components/buttons/BackButton";
+import MainButton from "../components/buttons/MainButton";
+import TopicButton from "../components/buttons/TopicButton";
+import Navbar from "../components/NavBar";
+import { CoreStyle } from "../components/CoreStyle";
+
+var last;
+var next;
+
+export default class GlobalBoard extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            scores: []
+        }
+        this.getScores();
+    };
+
+  async getScores() {
+    var n = [];
+    var s = [];
+    n = await AsyncStorage.getAllKeys();
+    for (var i = 0; i < n.length; i++) {
+        s.push([n[i], await AsyncStorage.getItem(n[i])]);
+    };
+    this.setState({
+        scores: s.sort((a,b)=> b[1] - a[1])
+    });
+  }
+
+    ItemView = ({ item }) => {
+        return (
+          // Flat List Item
+          <View style={CoreStyle.board}>
+            <Text style={CoreStyle.entry}>{item[0]}</Text>
+            <Text style={CoreStyle.entry}>{item[1]}</Text>
+          </View>
+        );
+    };
+
+    ItemSeparatorView = () => {
+        return (
+          // Flat List Item Separator
+          <View
+            style={{
+              height: 2,
+              width: '100%',
+              backgroundColor: 'rgba(52, 52, 52, 0.0)',
+            }}
+          />
+        );
+    };
+
+  handleBackNav = () => {
+    if (this.props.navigation.getParam("topic") != "Falls") {
+        this.props.navigation.navigate("LocalBoard", {topic: last});
+    } else {
+        this.props.navigation.navigate("Menu", {module:"LocalBoard"});
+    }
+  }
+
+  handleNextNav = () => {
+    if (this.props.navigation.getParam("topic") != "Parental Health") {
+        this.props.navigation.navigate("LocalBoard", {topic: next});
+    } else {
+        () => this.props.navigation.navigate("LocalBoard", {topic: "Parental Health"})
+    }
+  }
+
+  goMenu = () => {
+    this.props.navigation.navigate("Menu", {module: "LocalBoard"});
+  };
+
+    goInformation = (_topic) => {
+      this.props.navigation.navigate("Information", {topic: _topic});
+    };
+
+  render (){
+    switch(this.props.navigation.getParam('topic')) {
+        case "Falls":
+            last = "LocalBoard";
+            next = "Burns";
+            break;
+        case "Burns":
+            last = "Falls";
+            next = "Poisonings";
+            break;
+        case "Poisonings":
+            last = "Burns";
+            next = "Drownings";
+            break;
+        case "Drownings":
+            last =  "Poisonings";
+            next = "Car Safety";
+            break;
+        case "Car Safety":
+            last = "Drownings";
+            next = "Parental Health";
+            break;
+        case "Parental Health":
+            last = "Car Safety";
+            next = "Parental Health";
+            break;
+        default:
+            break
+    };
+
+    return (
+    <ImageBackground source={global.bg} style={CoreStyle.image}>
+
+    <Text allowFontScaling={true} style={CoreStyle.title}> {"Quiz Scores"}: </Text>
+
+    <SafeAreaView>
+        <FlatList style={CoreStyle.leaderboard}
+            data={this.state.scores}
+            renderItem={this.ItemView}
+            ItemSeparatorComponent={this.ItemSeparatorView}
+            keyExtractor={(item) => item}
+        />
+    </SafeAreaView>
+
+    <View style={CoreStyle.center}>
+        <MainButton
+              text={"Go to Quizzes"}
+              onPress={() => this.props.navigation.navigate("Menu", {module:"Quiz"})}
+              txtColor={global.text}
+        ></MainButton>
+    </View>
+
+    <View style = {CoreStyle.pushdown}>
+    <Navbar navigation={this.props.navigation}/>
+    </View>
+
+    </ImageBackground>
+    );
+  };
+}
--- src/screens/Level1.js
+++ src/screens/Level1.js
@@ -5,6 +5,7 @@
  */
 
 import React, { Component } from "react";
+import axios from "axios";
 import {Image, ImageBackground, Linking, Modal, StyleSheet, Text, View } from "react-native";
 import { GameEngine } from "react-native-game-engine";
 import Background from "../assets/living-room/level-1-floor.png";
@@ -82,6 +83,23 @@ export default class LevelOne extends Component {
   //callback to restart the level
   handleLevelRestart = () => {
     this.props.navigation.replace("LevelOne");
+  };
+
+  handleLeaderboard = () => {
+    var axios = require("axios").default;
+
+    var options = {
+      method: 'PATCH',
+      url: 'https://childsafe.us.auth0.com/api/v2/users/'+global.user_id,
+      headers: {authorization: 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InhsR0ZUWTZ5a283eHNMWVFnUVdxSSJ9.eyJpc3MiOiJodHRwczovL2NoaWxkc2FmZS51cy5hdXRoMC5jb20vIiwic3ViIjoiTU9hZENodU1LMTFocHBWWG5ibXNMQlJaVEdsVE9mWmhAY2xpZW50cyIsImF1ZCI6Imh0dHBzOi8vY2hpbGRzYWZlLnVzLmF1dGgwLmNvbS9hcGkvdjIvIiwiaWF0IjoxNjY1NDQ2MDczLCJleHAiOjE2NjU1MzI0NzMsImF6cCI6Ik1PYWRDaHVNSzExaHBwVlhuYm1zTEJSWlRHbFRPZlpoIiwic2NvcGUiOiJyZWFkOmNsaWVudF9ncmFudHMgY3JlYXRlOmNsaWVudF9ncmFudHMgZGVsZXRlOmNsaWVudF9ncmFudHMgdXBkYXRlOmNsaWVudF9ncmFudHMgcmVhZDp1c2VycyB1cGRhdGU6dXNlcnMgZGVsZXRlOnVzZXJzIGNyZWF0ZTp1c2VycyByZWFkOnVzZXJzX2FwcF9tZXRhZGF0YSB1cGRhdGU6dXNlcnNfYXBwX21ldGFkYXRhIGRlbGV0ZTp1c2Vyc19hcHBfbWV0YWRhdGEgY3JlYXRlOnVzZXJzX2FwcF9tZXRhZGF0YSByZWFkOnVzZXJfY3VzdG9tX2Jsb2NrcyBjcmVhdGU6dXNlcl9jdXN0b21fYmxvY2tzIGRlbGV0ZTp1c2VyX2N1c3RvbV9ibG9ja3MgY3JlYXRlOnVzZXJfdGlja2V0cyByZWFkOmNsaWVudHMgdXBkYXRlOmNsaWVudHMgZGVsZXRlOmNsaWVudHMgY3JlYXRlOmNsaWVudHMgcmVhZDpjbGllbnRfa2V5cyB1cGRhdGU6Y2xpZW50X2tleXMgZGVsZXRlOmNsaWVudF9rZXlzIGNyZWF0ZTpjbGllbnRfa2V5cyByZWFkOmNvbm5lY3Rpb25zIHVwZGF0ZTpjb25uZWN0aW9ucyBkZWxldGU6Y29ubmVjdGlvbnMgY3JlYXRlOmNvbm5lY3Rpb25zIHJlYWQ6cmVzb3VyY2Vfc2VydmVycyB1cGRhdGU6cmVzb3VyY2Vfc2VydmVycyBkZWxldGU6cmVzb3VyY2Vfc2VydmVycyBjcmVhdGU6cmVzb3VyY2Vfc2VydmVycyByZWFkOmRldmljZV9jcmVkZW50aWFscyB1cGRhdGU6ZGV2aWNlX2NyZWRlbnRpYWxzIGRlbGV0ZTpkZXZpY2VfY3JlZGVudGlhbHMgY3JlYXRlOmRldmljZV9jcmVkZW50aWFscyByZWFkOnJ1bGVzIHVwZGF0ZTpydWxlcyBkZWxldGU6cnVsZXMgY3JlYXRlOnJ1bGVzIHJlYWQ6cnVsZXNfY29uZmlncyB1cGRhdGU6cnVsZXNfY29uZmlncyBkZWxldGU6cnVsZXNfY29uZmlncyByZWFkOmhvb2tzIHVwZGF0ZTpob29rcyBkZWxldGU6aG9va3MgY3JlYXRlOmhvb2tzIHJlYWQ6YWN0aW9ucyB1cGRhdGU6YWN0aW9ucyBkZWxldGU6YWN0aW9ucyBjcmVhdGU6YWN0aW9ucyByZWFkOmVtYWlsX3Byb3ZpZGVyIHVwZGF0ZTplbWFpbF9wcm92aWRlciBkZWxldGU6ZW1haWxfcHJvdmlkZXIgY3JlYXRlOmVtYWlsX3Byb3ZpZGVyIGJsYWNrbGlzdDp0b2tlbnMgcmVhZDpzdGF0cyByZWFkOmluc2lnaHRzIHJlYWQ6dGVuYW50X3NldHRpbmdzIHVwZGF0ZTp0ZW5hbnRfc2V0dGluZ3MgcmVhZDpsb2dzIHJlYWQ6bG9nc191c2VycyByZWFkOnNoaWVsZHMgY3JlYXRlOnNoaWVsZHMgdXBkYXRlOnNoaWVsZHMgZGVsZXRlOnNoaWVsZHMgcmVhZDphbm9tYWx5X2Jsb2NrcyBkZWxldGU6YW5vbWFseV9ibG9ja3MgdXBkYXRlOnRyaWdnZXJzIHJlYWQ6dHJpZ2dlcnMgcmVhZDpncmFudHMgZGVsZXRlOmdyYW50cyByZWFkOmd1YXJkaWFuX2ZhY3RvcnMgdXBkYXRlOmd1YXJkaWFuX2ZhY3RvcnMgcmVhZDpndWFyZGlhbl9lbnJvbGxtZW50cyBkZWxldGU6Z3VhcmRpYW5fZW5yb2xsbWVudHMgY3JlYXRlOmd1YXJkaWFuX2Vucm9sbG1lbnRfdGlja2V0cyByZWFkOnVzZXJfaWRwX3Rva2VucyBjcmVhdGU6cGFzc3dvcmRzX2NoZWNraW5nX2pvYiBkZWxldGU6cGFzc3dvcmRzX2NoZWNraW5nX2pvYiByZWFkOmN1c3RvbV9kb21haW5zIGRlbGV0ZTpjdXN0b21fZG9tYWlucyBjcmVhdGU6Y3VzdG9tX2RvbWFpbnMgdXBkYXRlOmN1c3RvbV9kb21haW5zIHJlYWQ6ZW1haWxfdGVtcGxhdGVzIGNyZWF0ZTplbWFpbF90ZW1wbGF0ZXMgdXBkYXRlOmVtYWlsX3RlbXBsYXRlcyByZWFkOm1mYV9wb2xpY2llcyB1cGRhdGU6bWZhX3BvbGljaWVzIHJlYWQ6cm9sZXMgY3JlYXRlOnJvbGVzIGRlbGV0ZTpyb2xlcyB1cGRhdGU6cm9sZXMgcmVhZDpwcm9tcHRzIHVwZGF0ZTpwcm9tcHRzIHJlYWQ6YnJhbmRpbmcgdXBkYXRlOmJyYW5kaW5nIGRlbGV0ZTpicmFuZGluZyByZWFkOmxvZ19zdHJlYW1zIGNyZWF0ZTpsb2dfc3RyZWFtcyBkZWxldGU6bG9nX3N0cmVhbXMgdXBkYXRlOmxvZ19zdHJlYW1zIGNyZWF0ZTpzaWduaW5nX2tleXMgcmVhZDpzaWduaW5nX2tleXMgdXBkYXRlOnNpZ25pbmdfa2V5cyByZWFkOmxpbWl0cyB1cGRhdGU6bGltaXRzIGNyZWF0ZTpyb2xlX21lbWJlcnMgcmVhZDpyb2xlX21lbWJlcnMgZGVsZXRlOnJvbGVfbWVtYmVycyByZWFkOmVudGl0bGVtZW50cyByZWFkOmF0dGFja19wcm90ZWN0aW9uIHVwZGF0ZTphdHRhY2tfcHJvdGVjdGlvbiByZWFkOm9yZ2FuaXphdGlvbnNfc3VtbWFyeSBjcmVhdGU6YWN0aW9uc19sb2dfc2Vzc2lvbnMgcmVhZDpvcmdhbml6YXRpb25zIHVwZGF0ZTpvcmdhbml6YXRpb25zIGNyZWF0ZTpvcmdhbml6YXRpb25zIGRlbGV0ZTpvcmdhbml6YXRpb25zIGNyZWF0ZTpvcmdhbml6YXRpb25fbWVtYmVycyByZWFkOm9yZ2FuaXphdGlvbl9tZW1iZXJzIGRlbGV0ZTpvcmdhbml6YXRpb25fbWVtYmVycyBjcmVhdGU6b3JnYW5pemF0aW9uX2Nvbm5lY3Rpb25zIHJlYWQ6b3JnYW5pemF0aW9uX2Nvbm5lY3Rpb25zIHVwZGF0ZTpvcmdhbml6YXRpb25fY29ubmVjdGlvbnMgZGVsZXRlOm9yZ2FuaXphdGlvbl9jb25uZWN0aW9ucyBjcmVhdGU6b3JnYW5pemF0aW9uX21lbWJlcl9yb2xlcyByZWFkOm9yZ2FuaXphdGlvbl9tZW1iZXJfcm9sZXMgZGVsZXRlOm9yZ2FuaXphdGlvbl9tZW1iZXJfcm9sZXMgY3JlYXRlOm9yZ2FuaXphdGlvbl9pbnZpdGF0aW9ucyByZWFkOm9yZ2FuaXphdGlvbl9pbnZpdGF0aW9ucyBkZWxldGU6b3JnYW5pemF0aW9uX2ludml0YXRpb25zIiwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIn0.AdnjjyBAn4i4HfjbApn_QK-nqtyAUEtPQceMdPQq1wlA_SsSYWBGF4MTRG3dGfGh-Vci05JfF9ClXTeGwUb6dHzdwRCen6d4Q06YiOn2izy-QB1kkoTyOgPvuudBMLIv-fUO0lkDgtwpcqbwFVgOfPWQk16SpHP44DBPMG1NK5I9NF2JWHDqRg7Th7laOOElpuK2GFGypXOGcozN8ORMqQKU5ZDEg2YivDuoLQvy8ZT5qZ7ciFfwQAVtYrdX3rdMYq9-L8Q5n8mdy0VxuAoxxIIHQ9cclmoXMJtDkX4GTEFb0fbKZdxF_K-aJROBbPB2nh2qRtxhXQY0MwWGDZ2E0g'},
+      data: {user_metadata: {score1: this.state.min+":"+this.state.sec+":"+this.state.msec}}
+    };
+
+    axios.request(options).then(function (response) {
+      console.log(response.data);
+    }).catch(function (error) {
+      console.error(error);
+    });
   };
 
   // go home callback
@@ -230,6 +248,10 @@ export default class LevelOne extends Component {
                   <MenuButton
                     text="PLAY AGAIN"
                     onPress={this.handleLevelRestart}
+                  ></MenuButton>
+                  <MenuButton
+                    text="ENTER SCORE"
+                    onPress={this.handleLeaderboard}
                   ></MenuButton>
                   <MenuButton
                     text="QUIT"
